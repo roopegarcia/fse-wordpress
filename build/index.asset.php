fa27ed2c67378c43c55cb334decc436846e52be7
rogxn <roberto.garcia@generaxion.fi>
2024-03-02 19:33:39 +0200

    Block theme - really trying without ACF. @wordpress/scripts running and start building

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '57b1d3215a3d0c8b8479');
---
+++ functions.php
@@ -0,0 +1,41 @@
+<?php 
+
+function university_files() {
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyDin3iGCdZ7RPomFLyb2yqFERhs55dmfTI', NULL, '1.0', true);
+  wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
+
+  wp_localize_script('main-university-js', 'universityData', array(
+    'root_url' => get_site_url(),
+    'nonce' => wp_create_nonce('wp_rest')
+  ));
+
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features() {
+  add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400, 260, true);
+  add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
+  add_theme_support('editor-styles');
+  add_editor_style(array('https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i', 'build/style-index.css', 'build/index.css'));
+}
+
+add_action('after_setup_theme', 'university_features');
+
+
+
+function bannerBlock() {
+  wp_register_script('bannerBlockScript', get_stylesheet_directory_uri() . '/build/banner.js', array('wp-blocks', 'wp-editor'));
+  register_block_type("ourblocktheme/banner", array(
+    'editor_script' => 'bannerBlockScript'
+  ));
+}
+
+add_action('init', 'bannerBlock');
